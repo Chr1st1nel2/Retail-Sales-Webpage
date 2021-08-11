30963e996a4f77f09ddfcb88c313380ca71da4d6
chr1st1nel2 <jiawenl0730@gmail.com>
2021-08-11 16:02:57 -0700

    implemented info card

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles'
-import { Grid, Paper, AppBar, Toolbar } from '@material-ui/core'
+import React, { Fragment } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { Grid, Paper, AppBar, Toolbar, Card, Typography, Chip } from '@material-ui/core';
 import data from './stackline_frontend_assessment_data_2021.json';
 import logo from './stackline_logo.svg';
 import './App.css';
@@ -17,7 +17,31 @@ const useStyles = makeStyles((theme) => ({
     margin: '0px',
     paddingTop: '50px',
   },
-  paper: {
+  info: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    height:'100vh',
+    color: theme.palette.text.secondary,
+  },
+  title: {
+    padding: '20px 30px 5px',
+    fontWeight: 'bold',
+    color: '#212121'
+  },
+  subtitle: {
+    padding: '0px 30px 20px',
+    color: '#bdbdbd'
+  },
+  tag: {
+    background: 'white',
+    margin: '0px 3px 20px',
+  },
+  graph: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+  table: {
     padding: theme.spacing(2),
     textAlign: 'center',
     color: theme.palette.text.secondary,
@@ -28,22 +52,44 @@ function App() {
   const classes = useStyles();
 
   return (
-    <React.Fragment>
-      <AppBar position="static" className={classes.appbar}>
+    <Fragment>
+      <AppBar position='static' className={classes.appbar}>
         <Toolbar>
-          <img src={logo} alt="logo" className={classes.logo} />
+          <img src={logo} alt='logo' className={classes.logo} />
         </Toolbar>
       </AppBar>
 
-    <Grid container spacing={2} className={classes.grid}>
-      <Grid item xs={3} direction="row">
-        <Paper className={classes.paper}>1</Paper>
+      <Grid container spacing={2} className={classes.grid}>
+        {data.map(( data ) => (
+          <Grid item xs={3} className={classes.grid}>
+            <Card className={classes.info}>
+              <img src={data.image} alt='url' className={classes.logo} />
+              <Typography variant="h5" className={classes.title}>
+                {data.title}
+              </Typography>
+              <Typography variant="body2" className={classes.subtitle}>
+                {data.subtitle}
+              </Typography>
+              {data.tags.map((tag)=>(
+                <Chip label={tag} variant="outlined" className={classes.tag}/>
+              ))}
+            </Card>
+
+          </Grid>
+        ))}
+          <Grid item xs={9} className={classes.grid}>
+            <Grid container spacing={2} direction='column'>
+              <Grid item>
+                <Paper className={classes.graph}>1</Paper>
+              </Grid>
+              <Grid item>
+                <Paper className={classes.table}>2</Paper>
+              </Grid>
+            </Grid>
+          </Grid>
+
       </Grid>
-      <Grid item xs={9}>
-        <Paper className={classes.paper}>1</Paper>
-      </Grid>
-    </Grid>
-    </React.Fragment>
+    </Fragment>
   );
 }
 
